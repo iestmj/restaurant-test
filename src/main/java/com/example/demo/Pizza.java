421bc7768761db6edb58a46ad8f511003772ceb5
Iestyn <iestynmj@hotmail.co.uk>
2019-10-23 17:15:50 +0100

    First commit. Added Entity,Repo,Controller with working showAll

---
+++ src/main/java/com/example/demo/Pizza.java
@@ -0,0 +1,19 @@
+package com.example.demo;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+public class Pizza {
+		@Id
+		private String name;
+
+		public String getName() {
+			return name;
+		}
+
+		public void setName(String name) {
+			this.name = name;
+		}
+		
+}
---
+++ src/main/java/com/example/demo/PizzaRepo.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import java.util.ArrayList;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Repository;
+
+@Repository
+interface PizzaRepo extends JpaRepository<Pizza, Integer>{
+		public ArrayList<Pizza>findAll();
+		
+}
